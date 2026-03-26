5522cfe10d3d3f5ef5c8071f2bc7f313376154b0
KALPIT NAGAR <kalpitnagar312@gmail.com>
2026-03-26 17:07:58 +0530

    Add pattern programs and executables
    Add new pattern-printing examples under Basics/Pattern: p8_2.cpp (prints an inverted centered star triangle) and p9.cpp (prints a diamond made of stars). Include compiled binaries p8_2.exe and p9.exe. Also add an empty placeholder p10.cpp.

---
+++ Basics/Pattern/p8_2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N;
+    cin>>N;
+
+        // Outer loop for rows
+        for (int i = 0; i < N; i++) {
+            
+            // Print leading spaces (increases with row number)
+            for (int j = 0; j < i; j++) {
+                cout << " ";
+            }
+
+            // Print stars (decreases with row number)
+            // Formula: total stars = 2*N - (2*i + 1)
+            for (int j = 0; j < 2 * N - (2 * i + 1); j++) {
+                cout << "*";
+            }
+
+            // Print trailing spaces (same as leading spaces)
+            for (int j = 0; j < i; j++) {
+                cout << " ";
+            }
+            cout<<endl;
+            
+
+        }
+    }
---
+++ Basics/Pattern/p9.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    for (int i = 0; i < n; i++)
+    { // space
+        for (int a = 0; a < n - i - 1; a++)
+        {
+            cout << " ";
+        }
+        // star
+        for (int b = 0; b < (2 * i) + 1; b++)
+        {
+            cout << "*";
+        }
+        // space
+        for (int c = 0; c < n - i - 1; c++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+    
+     for (int i = n-1; i >=0; i--)
+    { // space
+        for (int a = 0; a < n - i - 1; a++)
+        {
+            cout << " ";
+        }
+        // star
+        for (int b = 0; b < (2 * i) + 1; b++)
+        {
+            cout << "*";
+        }
+        // space
+        for (int c = 0; c < n - i - 1; c++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
